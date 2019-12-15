1c2157981529ee22c61e6a5a1fa704075ddf7f35
David Jacobsen <dgrjacobsen@gmail.com>
2019-12-15 18:48:07 +0100

    Static Pages finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home' #root_url
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about' #static_pages_about_url
-  root 'application#hello'
+  get 'static_pages/contact'
 end
