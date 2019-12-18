ebf62e4fbc7dffda0e75b892ffdabfaeefe216a0
David Jacobsen <dgrjacobsen@gmail.com>
2019-12-18 01:37:02 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
 
 =begin
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home' #root_url
   get 'static_pages/home'
   get 'static_pages/help'
